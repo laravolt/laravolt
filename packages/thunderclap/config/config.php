55d69e3794510b58043b8a1e6eab51a9cb78c460
Bayu Hendra <bayu.hendra@javan.co.id>
2016-04-26 16:52:25 +0700

    fix template (view layout and redirect url)

--- packages/thunderclap/config/config.php
+++ packages/thunderclap/config/config.php
@@ -3,6 +3,12 @@
  * Set specific configuration variables here
  */
 return [
+    'columns' => [
+        'except' => ['id', 'created_at', 'updated_at', 'deleted_at', 'remember_token']
+    ],
+    'view' => [
+        'extends' => 'layout'
+    ],
     'routes'     => [
         'prefix'    => '',
         'middleware' => [],
