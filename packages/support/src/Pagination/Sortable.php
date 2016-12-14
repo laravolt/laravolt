fc998f4a37a6c2729ba8fc2214712c20f576d9fa
Bayu Hendra Winata <uyab.exe@gmail.com>
2016-12-14 10:11:38 +0700

    Update Sortable.php
    Sortable link can have third parameter to add additional attributes

--- packages/support/src/Pagination/Sortable.php
+++ packages/support/src/Pagination/Sortable.php
@@ -4,30 +4,45 @@ namespace Laravolt\Support\Pagination;
 class Sortable
 {
     protected static $mapping = [
-        'asc'   => 'ascending sorted',
-        'desc'  => 'descending sorted'
+        'asc'  => 'ascending sorted',
+        'desc' => 'descending sorted',
     ];
 
     public static function link($parameters)
     {
-        if (count($parameters) == 1) $parameters[1] = ucfirst($parameters[0]);
+        if (count($parameters) == 1) {
+            $parameters[1] = ucfirst($parameters[0]);
+        }
 
         $column = $parameters[0];
         $title = $parameters[1];
+        $attributes = array_get($parameters, 2, []);
 
         $class = '';
-        if (request('orderBy') == $column && in_array(request('sortedBy'), ['asc', 'desc']))
+        if (request('orderBy') == $column && in_array(request('sortedBy'), ['asc', 'desc'])) {
             $class = static::$mapping[request('sortedBy')];
-
+        }
 
         $queryString = [
-            'orderBy' => $column,
-            'sortedBy' => request('sortedBy') === 'asc' ? 'desc' : 'asc'
+            'orderBy'  => $column,
+            'sortedBy' => request('sortedBy') === 'asc' ? 'desc' : 'asc',
         ];
 
         $url = route(request()->route()->getName(), array_merge(request()->input(), $queryString));
 
-        return '<th class="' . $class . '"><a href="' . $url . '"' . '>' . htmlentities($title) . '</a></th>';
+        $attributes = static::renderAttributes($attributes);
 
+        return '<th class="selectable '.$class.'" '.$attributes.'><a href="'.$url.'"'.'>'.htmlentities($title).'</a></th>';
+    }
+
+    protected static function renderAttributes($attributes)
+    {
+        $result = '';
+
+        foreach ($attributes as $attribute => $value) {
+            $result .= " {$attribute}=\"{$value}\"";
+        }
+
+        return $result;
     }
 }
