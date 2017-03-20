ff7aa5e8181cc7d0628ab392d0faf19488a45010
Bayu Hendra <bayu.hendra@javan.co.id>
2017-03-21 06:57:01 +0700

    preserve old tags value on updating

--- packages/comma/resources/views/posts/edit.blade.php
+++ packages/comma/resources/views/posts/edit.blade.php
@@ -8,7 +8,7 @@
         {!! SemanticForm::text('title', $post->title)->label(trans('comma::post.attributes.title')) !!}
         {!! SemanticForm::select('category_id', $categories, $post->category_id)->label(trans('comma::post.attributes.category')) !!}
         {!! SemanticForm::textarea('content', $post->content)->label(trans('comma::post.attributes.content')) !!}
-        {!! SemanticForm::selectMultiple('tags[]', $tags, $post->tagList)->placeholder('')->label(trans('comma::post.attributes.tags')) !!}
+        {!! SemanticForm::selectMultiple('tags[]', $tags, old('tags', $post->tagList))->placeholder('')->label(trans('comma::post.attributes.tags')) !!}
         {!! SemanticForm::submit(trans('comma::post.action.submit'))->addClass('primary') !!}
         {!! SemanticForm::close() !!}
     </div>
