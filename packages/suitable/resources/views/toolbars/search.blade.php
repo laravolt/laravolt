de506404c02e3fe342fb1dade4d05c8bf0864cce
FarisGenis <faaris513@gmail.com>
2018-07-27 10:52:51 +0700

    update search with button (#7)
    * update search with button
    
    * styling

--- packages/suitable/resources/views/toolbars/search.blade.php
+++ packages/suitable/resources/views/toolbars/search.blade.php
@@ -1,8 +1,8 @@
 <div class="item">
     <form method="GET" action="{{ request()->fullUrl() }}">
-        <div class="ui transparent icon input">
+        <div class="ui transparent action input">
             <input class="prompt" name="{{ $search }}" value="{{ request($search) }}" type="text" placeholder="@lang('suitable::suitable.search_placeholder')">
-            <i class="search link icon"></i>
+            <button class="ui submit basic button icon b-0"><i class="search link icon"></i></button>
         </div>
     </form>
 </div>
