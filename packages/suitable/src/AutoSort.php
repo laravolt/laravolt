de6de6b41f2c6c9ab9f131018547791d9add3fba
Bayu Hendra Winata <uyab.exe@gmail.com>
2018-08-20 13:09:55 +0700

    add trait AutoSort by related(foreign) column

---
+++ packages/suitable/src/AutoSort.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Laravolt\Suitable;
+
+use Illuminate\Database\Eloquent\Builder;
+
+trait AutoSort
+{
+    public function scopeAutoSort(Builder $query)
+    {
+        $sortByKey = config('suitable.query_string.sort_by');
+        $sortDirectionKey = config('suitable.query_string.sort_direction');
+        $direction = request()->get($sortDirectionKey, 'asc');
+
+        if (request()->has($sortByKey)) {
+            $column = request()->get($sortByKey);
+            if (str_contains($column, '.')) {
+                $temp = explode('.', $column);
+                $relation = $this->{$temp[0]}();
+                $related = $relation->getRelated();
+                $table = $related->getTable();
+                $column = $temp[1];
+
+                $query->select($this->getTable().'.*');
+                $query->join($table, $relation->getForeignKey(), '=', $table.".".$relation->getOwnerKey());
+                $query->orderBy($table.".".$column, $direction);
+            } else {
+                $query->orderBy($column, $direction);
+            }
+        } else {
+            $query->latest();
+        }
+    }
+}
