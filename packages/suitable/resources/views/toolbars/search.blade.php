f960922e0a5da3a84c6ffb33d42d2ae5738836e9
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2019-05-22 16:20:53 +0700

    Revert "change to url()->current()"
    This reverts commit 45d7c85886f7f28239a935c3de84ce446b087eca.

--- packages/suitable/resources/views/toolbars/search.blade.php
+++ packages/suitable/resources/views/toolbars/search.blade.php
@@ -1,4 +1,4 @@
-<form method="GET" action="{{ url()->current() }}">
+<form method="GET" action="{{ request()->fullUrl() }}">
     <div class="ui transparent action input">
         <input class="prompt" name="{{ $name }}" value="{{ request($name) }}" type="text"
                placeholder="@lang('suitable::suitable.search_placeholder')">
