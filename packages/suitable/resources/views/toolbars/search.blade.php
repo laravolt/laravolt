81f2dedee9d98373453d8e04f96f6993a915dda9
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2019-05-24 08:28:49 +0700

    Enhance searchbox query
    Keep existing query string, except for pagination

--- packages/suitable/resources/views/toolbars/search.blade.php
+++ packages/suitable/resources/views/toolbars/search.blade.php
@@ -1,5 +1,8 @@
-<form method="GET" action="{{ request()->fullUrl() }}">
+<form method="GET" action="{{ url()->current() }}">
     <div class="ui transparent action input">
+        @foreach(collect(request()->query())->except('page', $name) as $queryString => $value)
+            <input type="hidden" name="{{ $queryString }}" value="{{ $value }}">
+        @endforeach
         <input class="prompt" name="{{ $name }}" value="{{ request($name) }}" type="text"
                placeholder="@lang('suitable::suitable.search_placeholder')">
         <button class="ui submit basic button icon b-0"><i class="search link icon"></i></button>
