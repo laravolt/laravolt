008da740c225645852709846b4dc07e4c8f1b347
Bayu Hendra Winata <uyab.exe@gmail.com>
2019-09-27 08:55:34 +0700

    add: handle fileuploader

--- packages/platform/src/Platform/Models/User.php
+++ packages/platform/src/Platform/Models/User.php
@@ -10,11 +10,14 @@ use Laravolt\Platform\Concerns\CanResetPassword;
 use Laravolt\Platform\Concerns\HasRoleAndPermission;
 use Laravolt\Contracts\CanChangePassword as CanChangePasswordContract;
 use Laravolt\Contracts\HasRoleAndPermission as HasRoleAndPermissionContract;
+use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
 
-class User extends BaseUser implements CanChangePasswordContract, HasRoleAndPermissionContract
+class User extends BaseUser implements CanChangePasswordContract, HasMedia, HasRoleAndPermissionContract
 {
     use CanChangePassword;
     use CanResetPassword;
+    use HasMediaTrait;
     use HasRoleAndPermission;
 
     protected $guarded = [];
