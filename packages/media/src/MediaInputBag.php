ecade07ea854aacb65f5e8b1ff0f3d4d04c500cf
Bayu Hendra Winata <uyab.exe@gmail.com>
2019-11-19 17:44:12 +0700

    handle multiple file upload

--- packages/media/src/MediaInputBag.php
+++ packages/media/src/MediaInputBag.php
@@ -8,6 +8,10 @@ class MediaInputBag
 {
     protected $key;
 
+    protected $files = [];
+
+    protected $holder;
+
     /**
      * MediaInputBag constructor.
      * @param $key
@@ -15,6 +19,8 @@ class MediaInputBag
     public function __construct($key)
     {
         $this->key = $key;
+        $this->files = request($this->key);
+        $this->holder = auth()->user();
     }
 
     public function toArray(): array
@@ -22,8 +28,16 @@ class MediaInputBag
         return collect(json_decode(request("_{$this->key}", '{}'), true))->flatten()->toArray();
     }
 
-    public function store(): array
+    public function store(string $collection = 'default'): array
     {
+        $key = $this->key;
+        $files = request()->file($key) ?? [];
+        $media = [];
+        foreach ($files as $file) {
+            $media[] = $this->holder->addMedia($file)->toMediaCollection($collection);
+        }
+
+        return $media;
     }
 
     public function delete(): bool
