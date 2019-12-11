84c4bca0b6a6255de1f414602333369fb6a3daf1
Bayu Hendra Winata <uyab.exe@gmail.com>
2019-12-11 17:08:20 +0700

    multirow: move template to bottom of the page
    To avoid collision with Vue

--- packages/semantic-form/resources/views/tabular.blade.php
+++ packages/semantic-form/resources/views/tabular.blade.php
@@ -13,22 +13,6 @@
     </tr>
     </thead>
     <tbody>
-
-    <template>
-        <tr>
-            @foreach($fields as $field)
-                <td>{!! $field->bindAttribute('name', "") !!}</td>
-            @endforeach
-            @if($allowRemoval)
-                <td>
-                    <button class="ui button icon mini" type="button" data-role="tabular-remove-row" tabindex="-1">
-                        <i class="icon remove"></i>
-                    </button>
-                </td>
-            @endif
-        </tr>
-    </template>
-
     @foreach($rows as $fields)
         <tr>
             @foreach($fields as $field)
@@ -48,7 +32,8 @@
     @if($allowAddition)
         <tr>
             <th colspan="{{ count($labels) + ($allowRemoval) }}">
-                <button class="ui button fluid" data-role="tabular-add-row" tabindex="-1"><i class="icon plus"></i>
+                <button class="ui button fluid" data-template="{{ $name }}" data-role="tabular-add-row" tabindex="-1"><i
+                            class="icon plus"></i>
                     Tambah
                 </button>
             </th>
@@ -58,26 +43,45 @@
 </table>
 
 @push('script')
+    <template for="{{ $name }}">
+        <tr>
+            @foreach($fields as $field)
+                <td>{!! $field->bindAttribute('name', "") !!}</td>
+            @endforeach
+            @if($allowRemoval)
+                <td>
+                    <button class="ui button icon mini" type="button" data-role="tabular-remove-row" tabindex="-1">
+                        <i class="icon remove"></i>
+                    </button>
+                </td>
+            @endif
+        </tr>
+    </template>
+
     <script>
-      let tabular = $('[data-role="tabular"]');
+      $(function () {
+        let tabular = $('[data-role="tabular"]');
 
-      tabular.on('click', '[data-role="tabular-remove-row"]', function (e) {
-        e.preventDefault();
-        let counter = $(e.delegateTarget).find('[data-role="rows-counter"]');
-        let parent = $(e.currentTarget).parents('tr');
-        parent.fadeOut("slow", function (e) {
-          parent.remove();
-          counter.val(counter.val() - 1);
+        tabular.on('click', '[data-role="tabular-remove-row"]', function (e) {
+          e.preventDefault();
+          let counter = $(e.delegateTarget).find('[data-role="rows-counter"]');
+          let parent = $(e.currentTarget).parents('tr');
+          parent.fadeOut("slow", function (e) {
+            parent.remove();
+            counter.val(counter.val() - 1);
+          });
         });
-      });
 
-      tabular.on('click', '[data-role="tabular-add-row"]', function (e) {
-        e.preventDefault();
-        let counter = $(e.delegateTarget).find('[data-role="rows-counter"]');
-        let template = $(e.delegateTarget).find('template').get(0);
-        var clone = document.importNode(template.content, true);
-        $(e.delegateTarget).find('tbody').append($(clone));
-        counter.val(parseInt(counter.val()) + 1);
+        tabular.on('click', '[data-role="tabular-add-row"]', function (e) {
+          e.preventDefault();
+          let templateName = $(e.currentTarget).data('template');
+          let counter = $(e.delegateTarget).find('[data-role="rows-counter"]');
+          let template = $('template[for="'+templateName+'"]').get(0);
+          var clone = document.importNode(template.content, true);
+          $(e.delegateTarget).find('tbody').append($(clone));
+          counter.val(parseInt(counter.val()) + 1);
+        });
+
       });
     </script>
 @endpush
