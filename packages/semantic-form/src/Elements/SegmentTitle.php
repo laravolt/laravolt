8c6d1b4907266bd5ebfbf7f479defd2c27294b98
Bayu Hendra Winata <uyab.exe@gmail.com>
2019-12-24 15:44:26 +0700

    change segment title template

--- packages/semantic-form/src/Elements/SegmentTitle.php
+++ packages/semantic-form/src/Elements/SegmentTitle.php
@@ -6,15 +6,40 @@ namespace Laravolt\SemanticForm\Elements;
 
 class SegmentTitle extends Wrapper
 {
-    public static $template = "<h3 class='ui blue ribbon label'>%s</h3>";
+    protected static $template = "<h3 class='ui blue ribbon label'>%s</h3>";
 
-    protected $openTag = '<h3 %s>';
+    public static function setTemplate(string $template)
+    {
+        static::$template = $template;
+    }
 
-    protected $closeTag = '</h3>';
+    public function render()
+    {
+        $element = clone $this;
 
-    protected $attributes = [
-        'class' => 'ui header',
-    ];
+        if ($this->label) {
+            $element->label = false;
+
+            $field = $this->decorateField(new Field($this->label, $element));
+            if ($control = $element->getPrimaryControl()) {
+                $field->addClass($control->fieldWidth);
+            }
+
+            return $field->render();
+        }
+
+        $element->beforeRender();
+
+
+        $html = '';
+        foreach ($element->controls as $control) {
+            $html .= sprintf(static::$template, $control);
+        }
+
+        $html .= $element->renderHint();
+
+        return $html;
+    }
 
     public function display()
     {
