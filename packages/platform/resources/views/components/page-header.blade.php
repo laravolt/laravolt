1a00827ac0ec273a8e972eed2bf077379e3cd857
Bayu Hendra Winata <uyab.exe@gmail.com>
2019-12-25 15:30:18 +0700

    set page attributes using @section and @push

--- packages/platform/resources/views/components/page-header.blade.php
+++ packages/platform/resources/views/components/page-header.blade.php
@@ -1,12 +1,13 @@
 <div class="ui menu secondary page-header">
     <div class="item">
-        <h2 class="ui header">{!! $title !!}</h2>
+        <h2 class="ui header">@yield('page.title', $title ?? "")</h2>
     </div>
     <div class="menu right">
         <div class="item">
-            @foreach($actions as $action)
+            @foreach($actions ?? [] as $action)
                 @includeWhen($action['visible'] ?? true, 'laravolt::components.button', ['action' => $action])
             @endforeach
+            @stack('page.actions')
         </div>
     </div>
 </div>
