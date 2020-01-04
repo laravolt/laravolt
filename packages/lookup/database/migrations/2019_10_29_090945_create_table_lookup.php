01cae8fb14315d1453434939f09a6b5e2e105e38
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-01-04 11:17:46 +0700

    fix mgirations

--- packages/lookup/database/migrations/2019_10_29_090945_create_table_lookup.php
+++ packages/lookup/database/migrations/2019_10_29_090945_create_table_lookup.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class TableLookup extends Migration
+class CreateTableLookup extends Migration
 {
     /**
      * Run the migrations.
