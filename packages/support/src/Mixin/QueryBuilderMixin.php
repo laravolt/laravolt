57ce1139a77cf2f0d7858d5b2b4b847db3ed5ef2
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-01-05 19:09:28 +0700

    register Database QueryBuilder mixin

---
+++ packages/support/src/Mixin/QueryBuilderMixin.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Laravolt\Support\Mixin;
+
+use Illuminate\Database\Query\Builder;
+
+class QueryBuilderMixin
+{
+    public function firstOrFail()
+    {
+        return function () {
+            $result = $this->first();
+            if (! $result) {
+                abort(404);
+            }
+
+            return $result;
+        };
+    }
+
+    public function whereLike()
+    {
+        return function ($attributes, ?string $searchTerm) {
+            if ($searchTerm) {
+                $searchTerm = strtolower($searchTerm);
+                $this->where(function (Builder $query) use ($attributes, $searchTerm) {
+                    foreach (Arr::wrap($attributes) as $column) {
+                        $query->orWhereRaw(sprintf("LOWER(%s) LIKE '%%%s%%'", $column, $searchTerm));
+                    }
+                });
+            }
+
+            return $this;
+        };
+    }
+
+    public function autoFilter()
+    {
+        return function () {
+            foreach (request('filter', []) as $column => $value) {
+                if ($value) {
+                    $this->whereLike($column, $value);
+                }
+            }
+
+            return $this;
+        };
+    }
+
+    public function autoSort()
+    {
+        return function ($sortByKey = 'sort', $sortDirectionKey = 'direction') {
+            $sortByKey = $sortByKey ?? config('suitable.query_string.sort_by');
+            $sortDirectionKey = $sortDirectionKey ?? config('suitable.query_string.sort_direction');
+            $direction = request()->get($sortDirectionKey, 'asc');
+
+            if (request()->has($sortByKey)) {
+                $column = request()->get($sortByKey);
+                $this->orderBy($column, $direction);
+            }
+
+            return $this;
+        };
+    }
+}
--- packages/support/src/SupportServiceProvider.php
+++ packages/support/src/SupportServiceProvider.php
@@ -1,16 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Laravolt\Support;
 
+use Illuminate\Database\Query\Builder;
 use Illuminate\Support\ServiceProvider;
+use Laravolt\Support\Mixin\QueryBuilderMixin;
 
 class SupportServiceProvider extends ServiceProvider
 {
     public function register()
     {
+        $this->registerMacro();
     }
 
     public function boot()
     {
+    }
+
+    protected function registerMacro()
+    {
+        Builder::mixin(new QueryBuilderMixin());
     }
 }
