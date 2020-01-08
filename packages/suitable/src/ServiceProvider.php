3567e3ac041a2f6c3b7fd21a752462e5e1b094a6
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-01-09 05:28:37 +0700

    cleanup

--- packages/suitable/src/ServiceProvider.php
+++ packages/suitable/src/ServiceProvider.php
@@ -40,7 +40,6 @@ class ServiceProvider extends BaseServiceProvider
         $this->registerViews();
         $this->registerConfigurations();
         $this->loadTranslationsFrom(realpath(__DIR__.'/../resources/lang'), 'suitable');
-        $this->registerMacro();
     }
 
     /**
@@ -75,49 +74,6 @@ class ServiceProvider extends BaseServiceProvider
         $this->publishes([
             $this->packagePath('config/config.php') => config_path('laravolt/suitable.php'),
         ], 'config');
-    }
-
-    /**
-     * Flexible where like search
-     *
-     * @see https://murze.be/searching-models-using-a-where-like-query-in-laravel
-     * @return void
-     */
-    protected function registerMacro()
-    {
-        EloquentBuilder::macro('whereLike', function ($attributes, ?string $searchTerm) {
-
-            if ($searchTerm === null) {
-                return $this;
-            }
-
-            $searchTerm = strtolower($searchTerm);
-            $this->where(function (EloquentBuilder $query) use ($attributes, $searchTerm) {
-                foreach (Arr::wrap($attributes) as $attribute) {
-                    $query->when(
-                        Str::contains($attribute, '.'),
-                        function (EloquentBuilder $query) use ($attribute, $searchTerm) {
-                            [$relationName, $relationAttribute] = explode('.', $attribute);
-
-                            $query->orWhereHas($relationName,
-                                function (EloquentBuilder $query) use ($relationAttribute, $searchTerm) {
-                                    $query->whereRaw(sprintf("LOWER(%s) LIKE '%%%s%%'", $relationAttribute, $searchTerm));
-                                });
-                        },
-                        function (EloquentBuilder $query) use ($attribute, $searchTerm) {
-                            $table = $query->getModel()->getTable();
-                            if (Str::contains($attribute, '->')) {
-                                $query->orWhere($attribute, 'like', "%$searchTerm%");
-                            } else {
-                                $query->orWhereRaw(sprintf("LOWER(%s.%s) LIKE '%%%s%%'", $table, $attribute, $searchTerm));
-                            }
-                        }
-                    );
-                }
-            });
-
-            return $this;
-        });
     }
 
     /**
