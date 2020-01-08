1665674235561bfa50ffbc7c2e23921fe524ce8c
damar <damar.huda@live.com>
2020-01-08 10:06:47 +0700

    commit dlu

---
+++ packages/workflow/src/Console/Commands/SyncModule.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Laravolt\Camunda\Console\Commands;
+
+use Illuminate\Console\Command;
+use Laravolt\Camunda\Models\Module;
+
+class SyncModule extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'camunda:sync-module {--prune}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sync module definitions';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        foreach (config('laravolt.workflow.modules') as $key => $module) {
+            if ($this->validateModule($module)) {
+                Module::updateOrCreate(
+                    ['key' => $key],
+                    ['label' => $module['label'], 'process_definition_key' => $module['process_definition_key']]
+                );
+            }
+        }
+
+        if ($this->option('prune')) {
+            $keys = collect(config('laravolt.workflow.modules'))->keys();
+            $deleted = 0;
+            if ($keys->isNotEmpty()) {
+                $deleted = Module::query()->whereNotIn('key', $keys)->delete();
+            }
+            $this->info(sprintf('%d modules pruned', $deleted));
+        }
+    }
+
+    protected function validateModule($module)
+    {
+        return is_array($module);
+    }
+}
