a3d0ce9131bfee50e15ecae8fd14593983a69b23
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-01-11 22:01:31 +0700

    add helper: capture_exception

--- packages/support/src/helpers.php
+++ packages/support/src/helpers.php
@@ -1,2 +1,17 @@
 <?php
 
+if (! function_exists('capture_exception')) {
+    /**
+     * Capture and report exception
+     *
+     * @param Throwable $exception
+     *
+     * @return string
+     */
+    function capture_exception(Throwable $exception)
+    {
+        if (app()->bound('sentry')) {
+            app('sentry')->captureException($exception);
+        }
+    }
+}
