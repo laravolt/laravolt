33f6de99009d5c70e64378f94c2fd0fb474753b4
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-01-11 21:15:29 +0700

    add workflow:make command

--- packages/workflow/src/Console/Commands/MakeCommand.php
+++ packages/workflow/src/Console/Commands/MakeCommand.php
@@ -3,6 +3,9 @@
 namespace Laravolt\Workflow\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 
 class MakeCommand extends Command
 {
@@ -25,7 +28,23 @@ class MakeCommand extends Command
     public function handle()
     {
         $name = $this->ask('Human friendly name');
-        $key = $this->ask('Process definition key');
-        //@TODO: generate config file template
+        $id = $this->ask('Identifier', Str::slug($name));
+        $processDefinitionKey = $this->ask('Process definition key');
+        $stub = __DIR__ . '/../../../stubs/config.php';
+        $target = config_path(sprintf('workflow-modules/%s.php', $id));
+        $targetDir = dirname($target);
+
+        $content = File::get($stub);
+        foreach (['name', 'processDefinitionKey'] as $var) {
+            $content = str_replace('$' . $var, $$var, $content);
+        }
+
+        if (! is_dir($targetDir)) {
+            File::makeDirectory($targetDir);
+        }
+
+        File::put($target, $content);
+
+        Artisan::call('workflow:sync-module', ['--prune' => true]);
     }
 }
