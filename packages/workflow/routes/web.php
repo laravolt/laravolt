6f913cdfc2f3a4644b0bca758083172505684f90
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-01-11 14:09:28 +0700

    fix namespace

--- packages/workflow/routes/web.php
+++ packages/workflow/routes/web.php
@@ -9,7 +9,7 @@ use Laravolt\Workflow\Controllers\TaskController;
 $router->group(
     [
         'prefix' => config('laravolt.workflow.routes.prefix'),
-        'as' => 'camunda::',
+        'as' => 'workflow::',
         'middleware' => config('laravolt.workflow.routes.middleware'),
     ],
     function ($router) {
