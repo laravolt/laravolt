ed06ffed7c30b496dc3237360f47b6a09453a221
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-01-21 16:09:54 +0700

    update TaskHistory::processInstance()

--- packages/camunda/src/Models/TaskHistory.php
+++ packages/camunda/src/Models/TaskHistory.php
@@ -4,16 +4,29 @@ declare(strict_types=1);
 
 namespace Laravolt\Camunda\Models;
 
-class TaskHistory extends Task
+use Illuminate\Support\Arr;
+
+class TaskHistory extends CamundaModel
 {
+    protected $processInstance;
+
     protected function modelUri(): string
     {
         return 'history/task/';
     }
 
+    public function processInstance(): ProcessInstanceHistory
+    {
+        if (! $this->processInstance && $this->processInstanceId) {
+            $this->processInstance = (new ProcessInstanceHistory($this->processInstanceId))->fetch();
+        }
+
+        return $this->processInstance;
+    }
+
     public function fetch()
     {
-        $attributes = $this->get('history/task?taskId=' . $this->id);
+        $attributes = Arr::first($this->get('history/task?taskId=' . $this->id));
 
         foreach ($attributes as $key => $value) {
             $this->{$key} = $value;
