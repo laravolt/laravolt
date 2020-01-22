b53ea36746956fd2dc9bcf2e2e5abcbd7299a5df
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-01-22 23:45:43 +0700

    add bash script

---
+++ bin/split.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+~/monorepo-tools/monorepo_split.sh \
+  camunda:packages/camunda \
+  comma:packages/comma \
+  epilog:packages/epilog \
+  lookup:packages/lookup \
+  mailkeeper:packages/mailkeeper \
+  media:packages/media \
+  menu:packages/menu \
+  platform:packages/platform \
+  semantic-form:packages/semantic-form \
+  suitable:packages/suitable \
+  support:packages/support \
+  thunderclap:packages/thunderclap \
+  workflow:packages/workflow
