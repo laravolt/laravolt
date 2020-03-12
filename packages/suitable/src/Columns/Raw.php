73f7e20edaaef12f95a79faa5f6abd9749581bb5
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-03-12 10:37:29 +0700

    enh(suitable): Raw column can accept non Closure field

--- packages/suitable/src/Columns/Raw.php
+++ packages/suitable/src/Columns/Raw.php
@@ -6,6 +6,10 @@ class Raw extends Column implements ColumnInterface
 {
     public function cell($cell, $collection, $loop)
     {
-        return call_user_func($this->field, $cell, $collection, $loop);
+        if ($this->field instanceof \Closure) {
+            return call_user_func($this->field, $cell, $collection, $loop);
+        }
+
+        return data_get($cell, $this->field);
     }
 }
