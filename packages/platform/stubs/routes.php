0465c96730d22c04bc7b00a17186c126bf363d70
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-04-17 09:11:12 +0700

    fix namespace

--- packages/platform/stubs/routes.php
+++ packages/platform/stubs/routes.php
@@ -1,4 +1,4 @@
 <?php
 
-Route::get('/', \App\Http\Controllers\Home::class)->name('home');
-Route::get('/dashboard', \App\Http\Controllers\Dashboard::class)->name('dashboard');
+Route::get('/', 'Home')->name('home');
+Route::get('/dashboard', 'Dashboard')->name('dashboard');
