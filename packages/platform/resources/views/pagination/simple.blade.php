1e6c7a13545a62acedd24cfbd0c9659bec7f3ca6
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-05-07 13:57:37 +0700

    fix pagination

--- packages/platform/resources/views/pagination/simple.blade.php
+++ packages/platform/resources/views/pagination/simple.blade.php
@@ -1,15 +1,15 @@
 <div class="ui pagination menu">
     <!-- Previous Page Link -->
     @if ($paginator->onFirstPage())
-        <div class="item disabled">@lang('suitable::pagination.previous')</div>
+        <div class="item disabled"><i class="icon left chevron"></i></div>
     @else
-        <a class="item" href="{{ $paginator->previousPageUrl() }}" rel="prev">@lang('suitable::pagination.previous')</a>
+        <a class="item" href="{{ $paginator->previousPageUrl() }}" rel="prev"><i class="icon left chevron"></i></a>
     @endif
 
 <!-- Next Page Link -->
     @if ($paginator->hasMorePages())
-        <a class="item" href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('suitable::pagination.next')</a>
+        <a class="item" href="{{ $paginator->nextPageUrl() }}" rel="next"><i class="icon right chevron"></i></a>
     @else
-        <div class="item disabled">@lang('suitable::pagination.next')</div>
+        <div class="item disabled"><i class="icon right chevron"></i></div>
     @endif
 </div>
