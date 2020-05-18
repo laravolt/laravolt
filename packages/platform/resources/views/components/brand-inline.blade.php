35c278285e3a7d4f5e115807b9a82885d14316f7
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-05-18 15:44:28 +0700

    styling

--- packages/platform/resources/views/components/brand-inline.blade.php
+++ packages/platform/resources/views/components/brand-inline.blade.php
@@ -1,5 +1,5 @@
-<div data-role="brand" class="ui segment basic center aligned p-y-2">
-    <h4 class="ui centered header">
+<div data-role="brand" class="ui segment basic p-2 m-b-0">
+    <h4 class="ui header">
         {{ config('laravolt.ui.brand_name') }}
     </h4>
 </div>
