5ad04828eef361db2bd33f0ca92f39eff45e3144
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-05-21 02:10:28 +0000

    Apply fixes from StyleCI

--- packages/platform/src/Platform/Components/LinkComponent.php
+++ packages/platform/src/Platform/Components/LinkComponent.php
@@ -33,6 +33,7 @@ class LinkComponent extends Component
 
     /**
      * Get the view / contents that represent the component.
+     *
      * @return \Illuminate\View\View|string
      */
     public function render()
