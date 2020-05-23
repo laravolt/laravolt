a9d026bf8452223794ecbfa37270cbf0ae0e620d
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-05-23 06:29:12 +0000

    Apply fixes from StyleCI

--- packages/platform/src/Platform/Components/ButtonComponent.php
+++ packages/platform/src/Platform/Components/ButtonComponent.php
@@ -35,6 +35,7 @@ class ButtonComponent extends Component
 
     /**
      * Get the view / contents that represent the component.
+     *
      * @return \Illuminate\View\View|string
      */
     public function render()
