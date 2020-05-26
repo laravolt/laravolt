e66c6f6aafb59939b675c67abf46fb9e0925366b
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-05-27 02:46:40 +0700

    theming

--- packages/platform/resources/views/layouts/auth.blade.php
+++ packages/platform/resources/views/layouts/auth.blade.php
@@ -2,24 +2,26 @@
 @section('body')
 
     <div class="layout--split">
-        <div data-role="inspire">
-            @include('laravolt::components.inspire')
-        </div>
-        <div data-role="main" class="ui pattern diagonal-lines">
-            <div data-role="content">
-                <div class="p-3">
 
-                    @include('laravolt::components.brand')
-                    <div class="ui divider hidden m-b-4"></div>
-
-                    @yield('content')
-
-                    <div class="ui divider hidden section"></div>
-                    <div class="ui divider hidden section"></div>
-                    <div class="ui divider hidden section"></div>
-                    <div class="ui divider hidden section"></div>
-                    <div class="ui divider hidden section"></div>
+        <div class="x-inspire">
+            <div class="x-inspire__content">
+                <div class="ui container text text-center p-3">
+                    @include('laravolt::components.inspire')
                 </div>
+            </div>
+        </div>
+
+
+        <div class="x-auth">
+            <div class="x-auth__content">
+                @include('laravolt::components.brand')
+
+                <div class="ui divider hidden m-b-4"></div>
+
+                @yield('content')
+
+                <div class="ui divider hidden m-b-5"></div>
+
             </div>
         </div>
     </div>
