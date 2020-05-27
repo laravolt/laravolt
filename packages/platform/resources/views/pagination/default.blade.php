3a01d58399476d50e1cbd21635deda284c6221a3
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-05-27 15:27:55 +0700

    fix pagination not showing on 2nd page

--- packages/platform/resources/views/pagination/default.blade.php
+++ packages/platform/resources/views/pagination/default.blade.php
@@ -1,36 +1,34 @@
-@if($paginator->hasMorePages())
-    <div class="ui pagination menu">
-        <!-- Previous Page Link -->
-        @if ($paginator->onFirstPage())
-            <div class="item disabled"><i class="icon left chevron"></i></div>
-        @else
-            <a class="item" href="{{ $paginator->previousPageUrl() }}" rel="prev"><i class="icon left chevron"></i></a>
+<div class="ui pagination menu">
+    <!-- Previous Page Link -->
+    @if ($paginator->onFirstPage())
+        <div class="item disabled"><i class="icon left chevron"></i></div>
+    @else
+        <a class="item" href="{{ $paginator->previousPageUrl() }}" rel="prev"><i class="icon left chevron"></i></a>
+    @endif
+
+<!-- Pagination Elements -->
+    @foreach ($elements as $element)
+    <!-- "Three Dots" Separator -->
+        @if (is_string($element))
+            <div class="item disabled"><span>{{ $element }}</span></div>
         @endif
 
-    <!-- Pagination Elements -->
-        @foreach ($elements as $element)
-        <!-- "Three Dots" Separator -->
-            @if (is_string($element))
-                <div class="item disabled"><span>{{ $element }}</span></div>
-            @endif
-
-        <!-- Array Of Links -->
-            @if (is_array($element))
-                @foreach ($element as $page => $url)
-                    @if ($page == $paginator->currentPage())
-                        <a class="item active">{{ $page }}</a>
-                    @else
-                        <a class="item" href="{{ $url }}">{{ $page }}</a>
-                    @endif
-                @endforeach
-            @endif
-        @endforeach
-
-    <!-- Next Page Link -->
-        @if ($paginator->hasMorePages())
-            <a class="item" href="{{ $paginator->nextPageUrl() }}" rel="next"><i class="icon right chevron"></i></a>
-        @else
-            <div class="item disabled"><i class="icon right chevron"></i></div>
+    <!-- Array Of Links -->
+        @if (is_array($element))
+            @foreach ($element as $page => $url)
+                @if ($page == $paginator->currentPage())
+                    <a class="item active">{{ $page }}</a>
+                @else
+                    <a class="item" href="{{ $url }}">{{ $page }}</a>
+                @endif
+            @endforeach
         @endif
-    </div>
-@endif
+    @endforeach
+
+<!-- Next Page Link -->
+    @if ($paginator->hasMorePages())
+        <a class="item" href="{{ $paginator->nextPageUrl() }}" rel="next"><i class="icon right chevron"></i></a>
+    @else
+        <div class="item disabled"><i class="icon right chevron"></i></div>
+    @endif
+</div>
