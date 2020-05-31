0ed7e5b9611e763d5496a386427fc263257d90b1
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-05-31 19:18:17 +0700

    component: label

--- packages/platform/resources/views/kitchen-sink/components/label.blade.php
+++ packages/platform/resources/views/kitchen-sink/components/label.blade.php
@@ -20,4 +20,19 @@
             </div>
         @endforeach
     </div>
+    <div class="ui horizontal list">
+        @foreach(config('laravolt.ui.colors') as $color => $hex)
+            <div class="item">
+                <x-label :color="'circular empty '.$color"></x-label>
+            </div>
+        @endforeach
+    </div>
+    <div class="divider"></div>
+    <div class="ui horizontal list">
+        @foreach(config('laravolt.ui.colors') as $color => $hex)
+            <div class="item">
+                <x-label :color="'circular empty basic '.$color"></x-label>
+            </div>
+        @endforeach
+    </div>
 </x-panel>
