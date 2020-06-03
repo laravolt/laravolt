8695afa372b7e69e15ed78de49471edf1b975738
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-06-03 17:48:05 +0700

    feat(table): Number column

---
+++ packages/suitable/src/Columns/Number.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Laravolt\Suitable\Columns;
+
+class Number extends Column implements ColumnInterface
+{
+    protected $headerAttributes = ['class' => 'right aligned'];
+    protected $cellAttributes = ['class' => 'right aligned'];
+
+    public function cell($cell, $collection, $loop)
+    {
+        return number_format((int) data_get($cell, $this->field), 0, ',', '.');
+    }
+}
