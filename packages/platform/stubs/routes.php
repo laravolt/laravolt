96c348e616ba5501fe2c41adaa0b7fd46a4bd51f
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-11-20 14:46:16 +0700

    fix routes namespace

--- packages/platform/stubs/routes.php
+++ packages/platform/stubs/routes.php
@@ -1,4 +1,7 @@
 <?php
 
-Route::get('/', 'Home')->name('home');
-Route::get('/dashboard', 'Dashboard')->name('dashboard')->middleware('auth');
+use App\Http\Controllers\Dashboard;
+use App\Http\Controllers\Home;
+
+Route::get('/', Home::class)->name('home');
+Route::get('/dashboard', Dashboard::class)->name('dashboard')->middleware('auth');
