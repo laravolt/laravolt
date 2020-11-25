fb51ab9881011b4f4639513a0273885e58066198
Bayu Hendra Winata <uyab.exe@gmail.com>
2020-11-25 10:26:26 +0700

    kitchen-sink: form

--- packages/platform/resources/views/kitchen-sink/components/form.blade.php
+++ packages/platform/resources/views/kitchen-sink/components/form.blade.php
@@ -1,3 +1,19 @@
+<div class="ui grid">
+    <div class="column">
+        <x-panel title="Form Fields">
+            {!! form()->open()->multipart() !!}
+            {!! form()->text('text')->label('Text') !!}
+            {!! form()->email('email')->label('Email') !!}
+            {!! form()->number('number')->label('Number') !!}
+            {!! form()->date('date')->label('Date') !!}
+            {!! form()->time('time')->label('Time') !!}
+            {!! form()->rupiah('rupiah1')->label('Rupiah') !!}
+            {!! form()->rupiah('rupiah2')->label('Rupiah (dengan koma)') !!}
+            {!! form()->coordinate('koordinat')->label('Koordinat') !!}
+            {!! form()->close() !!}
+        </x-panel>
+    </div>
+</div>
 <div class="ui grid equal width">
     <div class="column">
         <x-panel title="Horizontal Form">
