866c55f392f8391b05c97608dd4aab670c80662f
Qisthi Ramadhani <realramageek@gmail.com>
2020-11-26 14:25:44 +0700

    import user to App\Models (#71)
    * import user to App\Models
    
    * HasFactory missing

--- packages/platform/stubs/User.php
+++ packages/platform/stubs/User.php
@@ -1,13 +1,15 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Notifications\Notifiable;
 use Laravolt\Support\Traits\AutoFilter;
 use Laravolt\Support\Traits\AutoSort;
 
 class User extends \Laravolt\Platform\Models\User
 {
+    use HasFactory;
     use Notifiable;
     use AutoSort;
     use AutoFilter;
