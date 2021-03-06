d14433879aaa17ff09f2a010491fee7d9d27190c
Bayu Hendra Winata <uyab.exe@gmail.com>
2021-03-07 01:02:56 +0700

    release: dua layout halaman auth

--- packages/platform/resources/views/layouts/auth.blade.php
+++ packages/platform/resources/views/layouts/auth.blade.php
@@ -1,28 +1,30 @@
 @extends('laravolt::layouts.base')
 @section('body')
 
-    <div class="layout--split">
-
-        <div class="x-inspire" style="background-image: url('{!! asset('laravolt/img/wallpaper/animated-svg/blue.svg') !!}')">
-            <div class="x-inspire__content">
-                <div class="ui container text text-center p-4">
-                    @include('laravolt::components.inspire')
+    <div class="layout--auth is-boxed">
+        <div class="layout--auth__container">
+            <div class="x-inspire"
+                 style="background-image: url('{!! asset('laravolt/img/wallpaper/animated-svg/blue.svg') !!}')"
+            >
+                <div class="x-inspire__content">
+                    <div class="x-inspire__text">@include('laravolt::components.inspire')</div>
                 </div>
             </div>
-        </div>
 
 
-        <div class="x-auth">
-            <div class="x-auth__content p-5">
+            <div class="x-auth">
+                <div class="x-auth__content">
 
-                <x-volt-brand-image class="ui image centered"></x-volt-brand-image>
-                <div class="ui divider hidden"></div>
+                    <x-volt-brand-image class="ui image centered"></x-volt-brand-image>
+                    <div class="ui divider hidden"></div>
 
-                @yield('content')
+                    @yield('content')
 
-                <div class="ui divider hidden m-b-5"></div>
+                    <div class="ui divider hidden m-b-5"></div>
 
+                </div>
             </div>
+
         </div>
     </div>
 @endsection
