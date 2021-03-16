d6b0b72edf8f778000a36a97775e980cc5ddc288
Bayu Hendra Winata <uyab.exe@gmail.com>
2021-03-16 11:09:26 +0700

    feat(suitable): new column type => Url

---
+++ packages/suitable/src/Columns/Url.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Laravolt\Suitable\Columns;
+
+class Url extends Column implements ColumnInterface
+{
+    public function cell($cell, $collection, $loop)
+    {
+        $url = data_get($cell, $this->field);
+
+        return sprintf('<a themed href="%s">%s</a>', url($url), $url);
+    }
+}
