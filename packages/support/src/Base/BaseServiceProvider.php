b06df7b31d5216d4e4fafd5ede9fd8b7f40eb6a8
Qisthi Ramadhani <realramageek@gmail.com>
2021-03-21 14:48:39 +0700

    typo config for route

--- packages/support/src/Base/BaseServiceProvider.php
+++ packages/support/src/Base/BaseServiceProvider.php
@@ -49,8 +49,7 @@ abstract class BaseServiceProvider extends ServiceProvider
 
     protected function bootRoutes()
     {
-        if (Arr::get($this->config, 'routes.enabled') && ! $this->app->routesAreCached()) {
-            $router = $this->app['router'];
+        if (Arr::get($this->config, 'route.enabled') && ! $this->app->routesAreCached()) {
             require $this->packagePath('routes/web.php');
         }
 
