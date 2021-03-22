c34045220a178be06c385035d67ec362fea97ec7
Qisthi Ramadhani <realramageek@gmail.com>
2021-03-22 14:29:25 +0700

    missing action + beautify indentation (#100)

--- packages/platform/stubs/resources/views/auth/verify-email.blade.php
+++ packages/platform/stubs/resources/views/auth/verify-email.blade.php
@@ -1,5 +1,4 @@
 <x-laravolt::layout.auth>
-
     <h3 class="ui header horizontal divider section">@lang('Verifikasi Email')</h3>
 
     <div class="ui message p-3">
@@ -9,10 +8,10 @@
     </div>
 
     {!! form()->post(route('verification.send')) !!}
-    <x-laravolt::button class="fluid">@lang('Kirim Ulang Email Verifikasi')</x-laravolt::button>
+        <x-laravolt::button class="fluid">@lang('Kirim Ulang Email Verifikasi')</x-laravolt::button>
     {!! form()->close() !!}
+
     <div class="ui divider section"></div>
-    <x-laravolt::link :url="route('logout')" class="fluid">Logout</x-laravolt::link>
 
-
+    <x-laravolt::link :url="route('auth::logout')" class="fluid">Logout</x-laravolt::link>
 </x-laravolt::layout.auth>
