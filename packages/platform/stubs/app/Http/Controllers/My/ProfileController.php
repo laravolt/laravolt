7057d7a60d9746c03e00b3a99bc87f8c352e7199
Bayu Hendra Winata <uyab.exe@gmail.com>
2021-03-29 16:58:09 +0700

    fix(stubs): MyProfileController

--- packages/platform/stubs/app/Http/Controllers/My/ProfileController.php
+++ packages/platform/stubs/app/Http/Controllers/My/ProfileController.php
@@ -4,27 +4,19 @@ namespace App\Http\Controllers\My;
 
 use Illuminate\Routing\Controller;
 use Laravolt\Epicentrum\Http\Requests\My\Profile\Update;
-use Laravolt\Epicentrum\Repositories\RepositoryInterface;
 use Laravolt\Support\Contracts\TimezoneRepository;
 
 class ProfileController extends Controller
 {
-    /**
-     * @var UserRepositoryEloquent
-     */
-    private $repository;
-
     private $timezone;
 
     /**
      * PasswordController constructor.
      *
-     * @param RepositoryInterface $repository
      * @param TimezoneRepository  $timezone
      */
-    public function __construct(RepositoryInterface $repository, TimezoneRepository $timezone)
+    public function __construct(TimezoneRepository $timezone)
     {
-        $this->repository = $repository;
         $this->timezone = $timezone;
     }
 
