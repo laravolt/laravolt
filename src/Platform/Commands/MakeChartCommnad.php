11c2fc4a02221dfe73e83a749aa8a8db653ce24e
Bayu Hendra Winata <uyab.exe@gmail.com>
2021-06-03 14:13:42 +0700

    enhance make:chart command

--- src/Platform/Commands/MakeChartCommnad.php
+++ src/Platform/Commands/MakeChartCommnad.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace Laravolt\Platform\Commands;
 
 use Illuminate\Console\GeneratorCommand;
+use Illuminate\Support\Str;
+use Symfony\Component\Console\Helper\TableSeparator;
 
 class MakeChartCommnad extends GeneratorCommand
 {
@@ -13,6 +15,23 @@ class MakeChartCommnad extends GeneratorCommand
     protected $name = 'make:chart {name}';
 
     protected $description = 'Create a new chart component';
+
+    public function handle()
+    {
+        $defaultHandler = parent::handle();
+
+        $slug = Str::kebab($this->getNameInput());
+        $info = [
+            ['Class', '<info>'.$this->qualifyClass($this->getNameInput()).'</info>'],
+            new TableSeparator(),
+            ['Blade Component', "<info><livewire:chart.$slug /></info>"],
+            new TableSeparator(),
+            ['Blade Directive', "<info>@livewire('chart.$slug')</info>"],
+        ];
+        $this->table([], $info);
+
+        return $defaultHandler;
+    }
 
     protected function getStub()
     {
