1a1bee732bcd1173adda560aeb04ae30dd952631
Bayu Hendra Winata <uyab.exe@gmail.com>
2021-06-08 11:11:55 +0700

    feat(table): allow DB query builder as valid source

--- packages/suitable/src/Concerns/SourceResolver.php
+++ packages/suitable/src/Concerns/SourceResolver.php
@@ -14,7 +14,7 @@ trait SourceResolver
 {
     public function resolve($source)
     {
-        if ($source instanceof Builder) {
+        if ($source instanceof Builder || $source instanceof \Illuminate\Database\Query\Builder) {
             return $source->paginate(request('per_page', $this->perPage));
         } elseif (is_subclass_of($source, Model::class)) {
             return (new $source())->all();
