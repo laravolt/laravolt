24740e6fe9dc369d64b8263fbe4831cafe9fbc39
Bayu Hendra Winata <uyab.exe@gmail.com>
2021-07-07 07:32:18 +0700

    feat(table): new Dropdown column

---
+++ packages/suitable/src/Columns/Dropdown.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Laravolt\Suitable\Columns;
+
+use Illuminate\Support\Str;
+
+class Dropdown extends Column implements ColumnInterface
+{
+    protected $options = [];
+
+    public static function make($field = null, $header = null)
+    {
+        return parent::make($field, '');
+    }
+
+    public function item(string $label, string $url, string $icon = null): self
+    {
+        $this->options[] = compact('label', 'url', 'icon');
+
+        return $this;
+    }
+
+    public function divider(): self
+    {
+        $this->options[] = '-';
+
+        return $this;
+    }
+
+    public function cell($cell, $collection, $loop)
+    {
+        $color = config('laravolt.ui.color');
+
+        $this->options = [];
+        call_user_func($this->field, $this, $cell);
+
+        $menu = collect($this->options)->map(function ($item) {
+            if ($item === '-') {
+                return '<div class="divider"></div>';
+            } else {
+                return "<a class='item' href='{$item['url']}'><i class='icon {$item['icon']}'></i> {$item['label']}</a>";
+            }
+        })->implode('');
+
+        return <<<HTML
+        <div class="ui icon top right pointing dropdown secondary $color button small">
+          <i class="ellipsis horizontal icon"></i>
+          <div class="menu inverted">
+            $menu
+          </div>
+        </div>            
+        HTML;
+    }
+}
