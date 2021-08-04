dfc54f825d0a766e206e1ea6e45926bb0b99f201
Bayu Hendra Winata <uyab.exe@gmail.com>
2021-08-05 06:30:52 +0700

    fix dummy

--- packages/workflow/src/FieldFormatter/Camunda/UploaderFormatter.php
+++ packages/workflow/src/FieldFormatter/Camunda/UploaderFormatter.php
@@ -8,16 +8,6 @@ class UploaderFormatter
 {
     public function __invoke($key, $value)
     {
-        // TODO: remove dummy
-        return [
-            'value' => 'R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
-            'type'  => 'File',
-            'valueInfo' => [
-                'filename' => 'dummy.jpg',
-                'mimetype' => 'image/gif',
-                'encoding' => 'UTF-8',
-            ],
-        ];
-        // return ['value' => request()->media($key)->toJson(), 'type' => 'String'];
+        return ['value' => request()->media($key)->toJson(), 'type' => 'String'];
     }
 }
