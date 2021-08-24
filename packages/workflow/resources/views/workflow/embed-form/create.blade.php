277f9c35b5ea914a0e67149b203c20e411cd9abc
Bayu Hendra Winata <uyab.exe@gmail.com>
2021-08-24 10:25:23 +0700

    workflow: styling embed form

--- packages/workflow/resources/views/workflow/embed-form/create.blade.php
+++ packages/workflow/resources/views/workflow/embed-form/create.blade.php
@@ -1,12 +1,21 @@
 <x-volt-public>
-    <x-volt-panel :title="request('_title', __('Mulai Proses Baru'))" :icon="request('_icon', 'rocket')">
-        <div class="ui inverted dimmer">
-            <div class="ui text loader">Processing...</div>
-        </div>
-        {!! $module->startForm(route('workflow::form.store', ['module' => $module->id]), request()->all()) !!}
-    </x-volt-panel>
+    <div class="ui container">
+        <x-volt-panel :title="request('_title', __('Mulai Proses Baru'))" :icon="request('_icon', 'rocket')">
+            <div class="ui inverted dimmer">
+                <div class="ui text loader">Processing...</div>
+            </div>
+            {!! $module->startForm(route('workflow::form.store', ['module' => $module->id]), request()->all()) !!}
+        </x-volt-panel>
+    </div>
 
     @once
+        @push('style')
+            <style>
+                body {
+                    background-color: transparent !important;
+                }
+            </style>
+        @endpush
         @push('script')
             <script>
                 document.addEventListener("DOMContentLoaded", function (event) {
