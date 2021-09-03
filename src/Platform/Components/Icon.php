534fd9d3eb20792785c8ffd829bd9f48a1ca0f77
Bayu Hendra Winata <uyab.exe@gmail.com>
2021-09-03 07:56:40 +0700

    fix docblock

--- src/Platform/Components/Icon.php
+++ src/Platform/Components/Icon.php
@@ -21,7 +21,7 @@ class Icon extends Component
     /**
      * Get the view / contents that represent the component.
      *
-     * @return \Illuminate\View\View|string
+     * @return \Closure
      */
     public function render()
     {
