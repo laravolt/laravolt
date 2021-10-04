082a8b9cc317ebb1b3ca488d946e1506108f6b49
Syofyan Zuhad <sofyanzuhad2@gmail.com>
2021-10-05 03:21:04 +0700

    update: auth-middleware-group (#123)

--- stubs/routes/my.php
+++ stubs/routes/my.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-// My Profile
-Route::get('my/profile', [\App\Http\Controllers\My\ProfileController::class, 'edit'])->name('my::profile.edit');
-Route::put('my/profile', [\App\Http\Controllers\My\ProfileController::class, 'update'])->name('my::profile.update');
+Route::middleware(['auth'])->group(function () {
+    // My Profile
+    Route::get('my/profile', [\App\Http\Controllers\My\ProfileController::class, 'edit'])->name('my::profile.edit');
+    Route::put('my/profile', [\App\Http\Controllers\My\ProfileController::class, 'update'])->name('my::profile.update');
 
-// My Password
-Route::get('my/password', [\App\Http\Controllers\My\PasswordController::class, 'edit'])->name('my::password.edit');
-Route::post('my/password', [\App\Http\Controllers\My\PasswordController::class, 'update'])->name('my::password.update');
+    // My Password
+    Route::get('my/password', [\App\Http\Controllers\My\PasswordController::class, 'edit'])->name('my::password.edit');
+    Route::post('my/password', [\App\Http\Controllers\My\PasswordController::class, 'update'])->name('my::password.update');
+});
