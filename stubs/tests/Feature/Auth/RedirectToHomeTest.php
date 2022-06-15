678f25c141b914dd8c93b3e4b6defcd0084cc800
ramaID <rama@javan.co.id>
2022-06-15 11:12:50 +0700

    feature/code coverage middleware authenticated (#164)
    * feature: code coverage Http/Middleware/RedirectIfAuthenticated 100%
    
    * remove unused code

---
+++ stubs/tests/Feature/Auth/RedirectToHomeTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Route;
+use Tests\TestCase;
+
+class RedirectToHomeTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     */
+    public function it_can_redirected_to_home()
+    {
+        /** @var \Illuminate\Contracts\Auth\Authenticatable $user */
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $response = $this->get(route('auth::login.show'));
+
+        $response->assertRedirect(RouteServiceProvider::HOME);
+    }
+}
