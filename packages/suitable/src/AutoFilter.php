532304179bda7e31c63d5ee5361a501861893863
Bayu Hendra Winata <uyab.exe@gmail.com>
2022-09-02 09:23:47 +0700

    change(autofilter): only apply filter defined in Model

--- packages/suitable/src/AutoFilter.php
+++ packages/suitable/src/AutoFilter.php
@@ -4,11 +4,15 @@ namespace Laravolt\Suitable;
 
 use Illuminate\Database\Eloquent\Builder;
 
+/** @mixin Builder */
 trait AutoFilter
 {
     public function scopeAutoFilter(Builder $query, $filter = 'filter')
     {
-        foreach (request($filter, []) as $column => $value) {
+        // Only apply filter defined in $filterableColumns
+        $filterPayload = collect(request($filter, []))->only($this->filterableColumns ?? []);
+
+        foreach ($filterPayload as $column => $value) {
             if ($value) {
                 $query->whereLike($column, $value);
             }
