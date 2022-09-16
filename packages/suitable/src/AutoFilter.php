1d9fb3e3b91ca220818143cb271f00b5e2b25014
Bayu Hendra Winata <uyab.exe@gmail.com>
2022-09-16 14:33:43 +0700

    feat(autofilter): handle where in

--- packages/suitable/src/AutoFilter.php
+++ packages/suitable/src/AutoFilter.php
@@ -3,6 +3,7 @@
 namespace Laravolt\Suitable;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Arr;
 
 /** @mixin Builder */
 trait AutoFilter
@@ -13,8 +14,10 @@ trait AutoFilter
         $filterPayload = collect(request($filter, []))->only($this->filterableColumns ?? []);
 
         foreach ($filterPayload as $column => $value) {
-            if ($value) {
+            if (is_string($value)) {
                 $query->whereLike($column, $value);
+            } elseif (is_array($value)) {
+                $query->whereIn($column, Arr::flatten($value));
             }
         }
     }
