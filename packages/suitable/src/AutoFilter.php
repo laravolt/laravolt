055587d26c6f2a42bedaa7c7de9c67750cce1b8f
Bayu Hendra Winata <uyab.exe@gmail.com>
2022-09-19 21:10:22 +0700

    feat: auto filter using dot notation

--- packages/suitable/src/AutoFilter.php
+++ packages/suitable/src/AutoFilter.php
@@ -4,6 +4,7 @@ namespace Laravolt\Suitable;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 /** @mixin Builder */
 trait AutoFilter
@@ -14,8 +15,21 @@ trait AutoFilter
         $filterPayload = collect(request($filter, []))->only($this->filterableColumns ?? []);
 
         foreach ($filterPayload as $column => $value) {
+            if (Str::contains($column, '.')) {
+                $relationName = Str::beforeLast($column, '.');
+                $column = Str::afterLast($column, '.');
+
+                /** @phpstan-ignore-next-line */
+                $query->leftJoinRelationship($relationName);
+
+                // Get fully qualified column name
+                $lastRelationName = Str::afterLast($relationName, '.');
+                $relatedTable = $this->$lastRelationName()->getModel()->getTable();
+                $column = "$relatedTable.$column";
+            }
+
             if (is_string($value)) {
-                $query->whereLike($column, $value);
+                $query->where($column, $value);
             } elseif (is_array($value)) {
                 $query->whereIn($column, Arr::flatten($value));
             }
