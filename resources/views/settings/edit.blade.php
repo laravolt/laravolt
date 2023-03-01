865aade2b585e76bf8f6af7cfd3de872cb378518
rama <rama@javan.co.id>
2023-03-01 22:30:45 +0700

    fix: uploader displaying tag html

--- resources/views/settings/edit.blade.php
+++ resources/views/settings/edit.blade.php
@@ -1,8 +1,9 @@
 <x-volt-app :title="__('Settings')">
     <x-volt-panel title="Global Application Settings">
         {!! form()->put(route('platform::settings.update'))->horizontal() !!}
-        {!! form()->make(config('laravolt.platform.settings'))->bindValues(config('laravolt.ui'))->render() !!}
-        {!! form()->action(form()->submit(__('Simpan'))) !!}
+            {!! form()->make(config('laravolt.platform.settings'))->bindValues($config)->render() !!}
+
+            {!! form()->action(form()->submit(__('Simpan'))) !!}
         {!! form()->close() !!}
     </x-volt-panel>
 </x-volt-app>
