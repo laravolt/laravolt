73f3fdc408f8f194ae1730a63448a26bf602fec8
Qisthi Ramadhani <rama@qisthi.dev>
2025-02-18 16:19:07 +0800

    fix: null coalesce on show page (#223)

--- packages/semantic-form/resources/views/multirow/table.blade.php
+++ packages/semantic-form/resources/views/multirow/table.blade.php
@@ -1,19 +1,22 @@
 <table class="ui table">
     <thead>
-    <tr>
-        @foreach($headers as $header)
-            <th>{{ $header }}</th>
-        @endforeach
-    </tr>
-
-    </thead>
-    <tbody>
-    @foreach($data as $row)
         <tr>
-            @foreach($row as $cell)
-                <td>{{ $cell }}</td>
+            @foreach($headers ?? [] as $header)
+                <th>{{ $header }}</th>
             @endforeach
         </tr>
-    @endforeach
+    </thead>
+    <tbody>
+        @forelse ($data ?? [] as $row)
+            <tr>
+                @foreach($row as $cell)
+                    <td>{{ $cell }}</td>
+                @endforeach
+            </tr>
+        @empty
+            <tr colspan="{{ count($headers ?? []) }}">
+                <td>-</td>
+            </tr>
+        @endforelse
     </tbody>
 </table>
