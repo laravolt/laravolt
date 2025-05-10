481b6f9c85099c02d86573df843ef5dc9c053b91
Nasrul Abdullah Rizal <34498755+nasrularizal@users.noreply.github.com>
2025-05-10 16:16:46 +0700

    fix error laravolt:clap command in laravel 12 (#258)

--- packages/thunderclap/src/DBHelper.php
+++ packages/thunderclap/src/DBHelper.php
@@ -3,16 +3,35 @@
 namespace Laravolt\Thunderclap;
 
 use Illuminate\Support\Facades\DB;
+use Doctrine\DBAL\DriverManager;
 
 class DBHelper
 {
     public function listTables()
     {
-        $tables = DB::getDoctrineSchemaManager()->listTables();
+        $laravelConfig = DB::connection()->getConfig();
+
+        $connectionParams = [
+            'dbname' => $laravelConfig['database'],
+            'user' => $laravelConfig['username'],
+            'password' => $laravelConfig['password'],
+            'host' => $laravelConfig['host'],
+            'driver' => $laravelConfig['driver'],
+            'port' => $laravelConfig['port'] ?? 5432,
+            'charset' => $laravelConfig['charset'] ?? 'utf8',
+        ];
+
+        $doctrineConnection = DriverManager::getConnection($connectionParams);
+
+        // Gunakan SchemaManager dari Doctrine DBAL 3.x
+        $schemaManager = $doctrineConnection->createSchemaManager();
+
+        // Ambil daftar tabel
+        $tableNames = $schemaManager->listTableNames();
 
         $data = [];
-        foreach ($tables as $table) {
-            $data[] = $table->getName();
+        foreach ($tableNames as $table) {
+            $data[] = $table;
         }
 
         return $data;
@@ -20,7 +39,25 @@ class DBHelper
 
     public function listColumns($table)
     {
-        $columns = \DB::getDoctrineSchemaManager()->listTableColumns($table);
+        $laravelConfig = DB::connection()->getConfig();
+
+        $connectionParams = [
+            'dbname' => $laravelConfig['database'],
+            'user' => $laravelConfig['username'],
+            'password' => $laravelConfig['password'],
+            'host' => $laravelConfig['host'],
+            'driver' => $laravelConfig['driver'],
+            'port' => $laravelConfig['port'] ?? 5432,
+            'charset' => $laravelConfig['charset'] ?? 'utf8',
+        ];
+
+        // Koneksi Doctrine
+        $doctrineConnection = DriverManager::getConnection($connectionParams);
+
+        // Schema Manager
+        $schemaManager = $doctrineConnection->createSchemaManager();
+
+        $columns = $schemaManager->listTableColumns($table);
 
         $data = [];
         foreach ($columns as $column) {
