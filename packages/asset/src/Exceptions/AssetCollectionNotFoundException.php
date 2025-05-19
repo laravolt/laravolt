7e5111203b4e43d2d629ed173a1d34f8f12464e7
ramaID <rama@qisthi.dev>
2025-05-19 22:22:29 +0700

    fix: deprecated Ignition error page

--- packages/asset/src/Exceptions/AssetCollectionNotFoundException.php
+++ packages/asset/src/Exceptions/AssetCollectionNotFoundException.php
@@ -2,23 +2,6 @@
 
 namespace Laravolt\Asset\Exceptions;
 
-use Facade\IgnitionContracts\BaseSolution;
-use Facade\IgnitionContracts\ProvidesSolution;
-use Facade\IgnitionContracts\Solution;
-
-class AssetCollectionNotFoundException extends \Exception implements ProvidesSolution
+class AssetCollectionNotFoundException extends \Exception
 {
-    public function getSolution(): Solution
-    {
-        return BaseSolution::create('Asset collection tidak ditemukan!')
-            ->setSolutionDescription(
-                sprintf(
-                    'Silakan cek kembali file %s dan pastikan nama collection yang Anda maksud sudah terdaftar. '
-                    .'Jika file tersebut belum ada, jalankan "php artisan vendor:publish --tag=laravolt-config"',
-                    config_path('laravolt/asset.php')
-                )
-            )->setDocumentationLinks([
-                'Pelajari lebih lanjut apa itu asset collection' => 'https://laravolt.dev/docs/asset/',
-            ]);
-    }
 }
