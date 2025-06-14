dbc3afe227d1825d70dd01c40031c4d6e2aad085
ramaID <rama@qisthi.dev>
2025-06-14 23:41:52 +0700

    feat: RestfulSwalButton

---
+++ packages/suitable/src/Columns/RestfulSwalButton.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Laravolt\Suitable\Columns;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Str;
+
+class RestfulSwalButton extends RestfulButton
+{
+    protected $header = 'Action';
+
+    protected $additionalButton;
+
+    public function getHeader()
+    {
+        return $this->header ?: 'Action';
+    }
+
+    public function cell($data, $collection, $loop)
+    {
+        $actions = $this->buildActions($data);
+        $deleteConfirmation = $this->buildDeleteConfirmation($data);
+        $key = Str::kebab(get_class($data)).'-'.$data->getKey();
+        $additionalButton = $this->buildAdditionalButton($data);
+
+        return View::make('suitable::columns.restful_swal_button', compact('data', 'actions', 'deleteConfirmation', 'key', 'additionalButton'))
+            ->render();
+    }
+
+    protected function buildAdditionalButton($data)
+    {
+        return $this->additionalButton;
+    }
+
+    public function additionalButton($label, $target)
+    {
+        $this->additionalButton = [
+            'label' => $label,
+            'target' => $target,
+        ];
+
+        return $this;
+    }
+}
