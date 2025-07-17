e04c3751498e8ccf014a8521877a1d1e5f3c7dc7
Qisthi Ramadhani <rama@qisthi.dev>
2025-07-17 11:41:45 +0700

    fix: colspan on empty state (#303)

--- resources/views/ui-component/table-view/empty.blade.php
+++ resources/views/ui-component/table-view/empty.blade.php
@@ -1,5 +1,5 @@
 <tr>
-    <td colspan="{{ count($columns) }}" class="p-0">
+    <td colspan="{{ count($columns)+1 }}" class="p-0">
         <div class="ui segment placeholder b-0 center aligned">
             <div class="text-gray-400 text-xl">
                 {{ __('No results') }}
