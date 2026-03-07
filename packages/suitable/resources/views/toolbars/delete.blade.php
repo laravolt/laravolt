89ac240e6dad9b220467248043a6608c7e6682c7
Qisthi Ramadhani <dizzy.leaps_0f@icloud.com>
2026-03-08 04:21:35 +0700

    feat(suitable): migrate delete toolbar view to Preline UI + Tailwind CSS

--- packages/suitable/resources/views/toolbars/delete.blade.php
+++ packages/suitable/resources/views/toolbars/delete.blade.php
@@ -1,5 +1,7 @@
 <form role="form" data-type="delete-multiple" action="{{ $url }}" method="POST" onsubmit="return confirm('Anda yakin?')">
     <input type="hidden" name="_method" value="DELETE">
     {{ csrf_field() }}
-    <button type="submit" class="ui button icon disabled"><i class="icon trash"></i></button>
+    <button type="submit" class="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700" disabled>
+        <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"/></svg>
+    </button>
 </form>
