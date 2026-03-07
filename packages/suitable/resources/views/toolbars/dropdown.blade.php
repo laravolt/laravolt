578ca5486fc8be56c18e46f4d9e9783b45daf373
Qisthi Ramadhani <dizzy.leaps_0f@icloud.com>
2026-03-08 04:21:29 +0700

    feat(suitable): migrate dropdown toolbar view to Preline UI + Tailwind CSS

--- packages/suitable/resources/views/toolbars/dropdown.blade.php
+++ packages/suitable/resources/views/toolbars/dropdown.blade.php
@@ -1,9 +1,13 @@
-<div class="ui dropdown">
-    <div class="text">{{ $options[request($name)] ?? $label }}</div>
-    <i class="dropdown icon"></i>
-    <div class="menu">
-        @foreach($options as $key => $value)
-            <a class="item" href="{{ url()->current() }}?{{ $name }}={{ $key }}">{{ $value }}</a>
-        @endforeach
+<div class="hs-dropdown relative inline-flex">
+    <button id="hs-dropdown-suitable-toolbar" type="button" class="hs-dropdown-toggle py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">
+        {{ $options[request($name)] ?? $label }}
+        <svg class="hs-dropdown-open:rotate-180 size-4 transition" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5"/></svg>
+    </button>
+    <div class="hs-dropdown-menu hs-dropdown-open:opacity-100 w-48 hidden z-10 transition-[opacity,margin] duration opacity-0 mt-2 bg-white shadow-md rounded-lg dark:bg-neutral-800 dark:border dark:border-neutral-700" role="menu">
+        <div class="p-1 space-y-0.5">
+            @foreach($options as $key => $value)
+                <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-300 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 {{ request($name) == $key ? 'bg-gray-100 dark:bg-neutral-700' : '' }}" href="{{ url()->current() }}?{{ $name }}={{ $key }}">{{ $value }}</a>
+            @endforeach
+        </div>
     </div>
 </div>
